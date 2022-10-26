a83df20a23a7008f68334f5989d25ed1f7b91157
MertOzerdem <32201738+MertOzerdem@users.noreply.github.com>
2022-10-27 02:33:17 +0300

    s 16 17

--- src/16-omit-and-pick.problem.ts
+++ src/16-omit-and-pick.problem.ts
@@ -11,6 +11,9 @@ interface User {
  * firstName and lastName properties of User?
  */
 
-type MyType = unknown;
+// type MyType = Pick<User, 'firstName' | 'lastName'>;
+
+// OR 
+type MyType = Omit<User, 'id'>;
 
 type tests = [Expect<Equal<MyType, { firstName: string; lastName: string }>>];
